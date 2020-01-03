13855a55c3d1f08aee120544c8c0d48352a89615
Farid <faridhlmd02@gmail.com>
2020-01-03 13:24:40 +0700

    indocement

---
+++ database/migrations/2020_01_02_094753_create_table_user.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('lokasi');
+            $table->string('jabatan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_user');
+    }
+}
---
+++ database/migrations/2020_01_02_094852_create_table_location.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableLocation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_location', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama_toko');
+            $table->string('kecamatan');
+            $table->string('kabupaten');
+            $table->decimal('lat1');
+            $table->decimal('long1');
+            $table->decimal('lat2');
+            $table->decimal('long2');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_location');
+    }
+}
