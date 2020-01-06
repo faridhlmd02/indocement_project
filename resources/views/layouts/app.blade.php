337e25da73353a00b17d6dc4c7c8174361f96286
Farid <faridhlmd02@gmail.com>
2020-01-06 17:15:20 +0700

    update language

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,7 @@
     <footer class="sticky-footer bg-white">
         <div class="container my-auto">
             <div class="copyright text-center my-auto">
-                <span>Copyright &copy; Your Website 2019</span>
+                <span>Copyright &copy; Indocement Project</span>
             </div>
         </div>
     </footer>
