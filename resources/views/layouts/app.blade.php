381a78d87183df6795c5581a888986875e35204a
Farid <faridhlmd02@gmail.com>
2020-01-06 13:11:33 +0700

    nih wang

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,113 +1,117 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
 
-        <title>SB Admin 2 - Dashboard</title>
+<head>
 
-        <!-- Custom fonts for this template-->
-        <link
-            href="{{url('sbadmin/vendor/fontawesome-free/css/all.min.css')}}"
-            rel="stylesheet"
-            type="text/css"
-        />
-        <link
-            href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-            rel="stylesheet"
-        />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <!-- Custom styles for this template-->
-        <link href="{{url('sbadmin/css/sb-admin-2.min.css')}}" rel="stylesheet" />
-    </head>
+    <title>SB Admin 2 - Dashboard</title>
 
-    <body id="page-top">
-        <!-- Page Wrapper -->
-        <div id="wrapper">
-            @include('layouts.topbar')
-            
-            @include('layouts.sidebar')
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('sbadmin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
-            <!-- Content Wrapper -->
-            <div id="content-wrapper" class="d-flex flex-column">
-                <!-- Main Content -->
-                <div id="content">
-                    @yield('content')
-                </div>
-                <!-- End of Main Content -->
+    <!-- Custom styles for this template-->
+    <link href="{{asset('sbadmin/css/sb-admin-2.min.css')}}" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        @include('layouts.sidebar')
+
+        @include('layouts.topbar')
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">Main Pages</h1>
             </div>
-            <!-- End of Content Wrapper -->
-        </div>
-        <!-- End of Page Wrapper -->
 
-        <!-- Scroll to Top Button-->
-        <a class="scroll-to-top rounded" href="#page-top">
-            <i class="fas fa-angle-up"></i>
-        </a>
+            <!-- Content Row -->
+            <div class="row">
 
-        <!-- Logout Modal-->
-        <div
-            class="modal fade"
-            id="logoutModal"
-            tabindex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
-        >
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">
-                            Ready to Leave?
-                        </h5>
-                        <button
-                            class="close"
-                            type="button"
-                            data-dismiss="modal"
-                            aria-label="Close"
-                        >
-                            <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        Select "Logout" below if you are ready to end your
-                        current session.
-                    </div>
-                    <div class="modal-footer">
-                        <button
-                            class="btn btn-secondary"
-                            type="button"
-                            data-dismiss="modal"
-                        >
-                            Cancel
-                        </button>
-                        <a class="btn btn-primary" href="login.html">Logout</a>
-                    </div>
+                <!-- Earnings (Monthly) Card Example -->
+                <div class="col-xl-3 col-md-6 mb-4">
+
                 </div>
             </div>
+
         </div>
+        <!-- /.container-fluid -->
 
-        <!-- Bootstrap core JavaScript-->
-        <script src="{{url('sbadmin/vendor/jquery/jquery.min.js')}}"></script>
-        <script src="{{url('sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    </div>
+    <!-- End of Main Content -->
 
-        <!-- Core plugin JavaScript-->
-        <script src="{{url('sbadmin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+    <!-- Footer -->
+    <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+                <span>Copyright &copy; Your Website 2019</span>
+            </div>
+        </div>
+    </footer>
+    <!-- End of Footer -->
 
-        <!-- Custom scripts for all pages-->
-        <script src="{{url('sbadmin/js/sb-admin-2.min.js')}}"></script>
+    </div>
+    <!-- End of Content Wrapper -->
 
-        <!-- Page level plugins -->
-        <script src="{{url('sbadmin/vendor/chart.js/Chart.min.js')}}"></script>
+    </div>
+    <!-- End of Page Wrapper -->
 
-        <!-- Page level custom scripts -->
-        <script src="{{url('sbadmin/js/demo/chart-area-demo.js')}}"></script>
-        <script src="{{url('sbadmin/js/demo/chart-pie-demo.js')}}"></script>
-    </body>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login.html">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('sbadmin/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('sbadmin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('sbadmin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('sbadmin/js/sb-admin-2.min.js')}}"></script>
+
+    <!-- Page level plugins -->
+    <script src="{{asset('sbadmin/vendor/chart.js/Chart.min.js')}}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{asset('sbadmin/js/demo/chart-area-demo.js')}}"></script>
+    <script src="{{asset('sbadmin/js/demo/chart-pie-demo.js')}}"></script>
+
+</body>
+
 </html>
